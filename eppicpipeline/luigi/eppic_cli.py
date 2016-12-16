f1803883c7fcb3563fa697ea650e132d73612788
Spencer Bliven <sbliven@ucsd.edu>
2016-12-16 14:12:13 +0100

    Run EppicCLI with luigi
    luigi --module eppicpipeline.luigi.eppic_cli EppicCli --local-scheduler --pdb=2gs2 --out=2gs2 --log=2gs2.log

--- eppicpipeline/luigi/eppic_cli.py
+++ eppicpipeline/luigi/eppic_cli.py
@@ -2,21 +2,56 @@ import luigi
 import eppic_config
 from pkg_resources import resource_string#, resource_stream, resource_listdir
 import pybars as hb
+import subprocess
 
 config = eppic_config.EppicConfig()
 
 class EppicCli(luigi.Task):
-    jar = luigi.Parameter(default="{}/eppic-cli/target/uber-eppic-cli-{}.jar".format(config.eppic_source_dir, config.eppic_version))
+    #input; required
+    pdb = luigi.Parameter()
+    out = luigi.Parameter(default="{}/data/divided/{{mid2}}/{{pdb}}".format(
+        config.wui_files)
+    )
+    log = luigi.Parameter(default=None)
+    jar = luigi.Parameter(default="{}/eppic-cli/target/uber-eppic-cli-{}.jar".format(
+        config.eppic_source_dir, config.eppic_version))
 
     def requires(self):
         return CreateEppicConfig()
 
+    def outputdir(self):
+        return str(self.out).format(mid2=self.pdb[1:3],pdb=self.pdb)
     def output(self):
-        return luigi.LocalTarget("eppic_cli.out")
+        return {
+            "log":luigi.LocalTarget(self.log) if self.log is not None else None,
+            "dir":luigi.LocalTarget(self.outputdir()),
+            "finished":luigi.LocalTarget("{}/finished".format(self.outputdir()) )
+            }
 
     def run(self):
-        with self.output().open('w') as out:
-            out.write("Jar={}\n".format(self.jar))
+        conf = self.input()
+        log = self.output()["log"]
+
+        cmd = ["java",
+            "-Xmx3g","-Xmn1g",
+            "-jar",str(self.jar),
+            "-i", str(self.pdb),
+            "-o", str(self.outputdir()),
+            "-a","1", #threads
+            "-w", #webui.dat
+            "-g",conf.open().name, #Is there a way to get the path without opening it?
+            "-l", #pymol images
+            #"-s", #entropy scores
+            "-P", #assembly diagrams
+            "-p", #interface coordinates
+        ]
+        if log is not None:
+            with self.output()["log"].open('w') as out:
+                out.write("CMD: "+" ".join(cmd))
+                return subprocess.call(cmd,stdout=out,stderr=subprocess.STDOUT)
+        else:
+            return subprocess.call(cmd)
+
 
 class CreateEppicConfig(luigi.Task):
     eppic_conf_file = luigi.Parameter(default="eppic_{}.conf".format(config.db))
