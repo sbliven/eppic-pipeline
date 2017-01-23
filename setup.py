166ce20ad45902a7afb51a43ed97023cf4e2e5d9
Spencer Bliven <spencer.bliven@gmail.com>
2017-01-23 16:32:55 +0100

    Switching version to be <pipeline-version>.<eppic-version>

--- setup.py
+++ setup.py
@@ -3,9 +3,12 @@
 from setuptools import setup, find_packages
 
 setup(name="eppicpipeline",
-    version="3.0.0-0",
+    #Pipeline version plus eppic version
+    version="0.0.1.3.0.0",
     description="Tools for running EPPIC",
     url="http://github.com/eppic/eppic-pipeline",
+    author="Spencer Bliven",
+    author_email="eppic@systemsx.ch",
     license="GPL2",
     install_requires=[
         "luigi  == 2.3.3",
